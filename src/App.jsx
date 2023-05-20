5bef3ca84202e300933c40e867d2ccdd898e2163
mohammadjalizi <mjalizi1376@gmail.com>
2023-05-20 20:38:59 +0330

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from './assets/navbar/Navbar'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+    
+    
+    mohammadjalizi
+    
+    </>
+  )
+}
+
+export default App
